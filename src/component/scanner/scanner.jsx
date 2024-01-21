b681e1a354f10dd93ad8f7c55a63942a81245350
Akash <sgc.akash.22oct.11sci@gmail.com>
2024-01-21 18:31:28 +0530

    Enhanced project documentation: Added a comprehensive PowerPoint presentation to better illustrate our Google solution and improve overall understanding.

--- src/component/scanner/scanner.jsx
+++ src/component/scanner/scanner.jsx
@@ -1,41 +1,48 @@
 import Navbar from "../../utils/navbar/navbar";
 import SearchBar from "../../utils/searchBar/articleSearch/searchBar";
 import CameraIcon from "../../assets/cameraIcon.svg";
+import UploadIcon from "../../assets/upload.svg";
+import FastIcon from "../../assets/fastIcon.svg";
+import securityIcon from "../../assets/securityIcon.svg";
 import "./scanner.css";
 import BarImgSearch from "../../utils/searchBar/barCode/searchBarCode";
+import Footer from "../../utils/footer/footer";
 export default function Scanner() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <section className="scannerContainer">
-        <div className="info">
-          <h1>EcoScan</h1>
-          <p>Uncover Sustainability at a Scan</p>
-        </div>
-        <div className="scannerOptions">
-          <SearchBar></SearchBar>
-          <button className="scannerIcon">
-            <img src={CameraIcon}></img>
-          </button>
-          <BarImgSearch></BarImgSearch>
-        </div>
-        <div className="quality">
-          <ul>
-            <li>
-              <img></img>
-              <p></p>
-            </li>
-            <li>
-              <img></img>
-              <p></p>
-            </li>
-            <li>
-              <img></img>
-              <p></p>
-            </li>
-          </ul>
-        </div>
-      </section>
-    </div>
+    <>
+      <div className="ScannerPage">
+        <Navbar></Navbar>
+        <section className="scannerContainer">
+          <div className="info">
+            <h1>EcoScan</h1>
+            <p>Uncover Sustainability at a Scan</p>
+          </div>
+          <div className="scannerOptions">
+            <SearchBar></SearchBar>
+            <button className="scannerIcon">
+              <img src={CameraIcon}></img>
+            </button>
+            <BarImgSearch></BarImgSearch>
+          </div>
+          <div className="services">
+            <ul>
+              <li>
+                <img src={UploadIcon}></img>
+                <p>10+ formats supported</p>
+              </li>
+              <li>
+                <img src={FastIcon}></img>
+                <p>Fast & Easy</p>
+              </li>
+              <li>
+                <img src={securityIcon}></img>
+                <p>Security Guaranteed</p>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+      <Footer></Footer>
+    </>
   );
 }
--- src/utils/footer/footer.jsx
+++ src/utils/footer/footer.jsx
@@ -1,4 +1,40 @@
+import facebookIcon from "../../assets/facebookIcon.svg";
+import instagramIcon from "../../assets/instagramIcon.svg";
+import twitterIcon from "../../assets/twitterIcon.svg";
 import "./footer.css";
 export default function Footer(props) {
-  return <footer>I am navbar</footer>;
+  return (
+    <footer>
+      <section>
+        <h1>Get In Touch</h1>
+        <ul>
+          <li>
+            <a title="facebook">
+              <img src={facebookIcon}></img>
+            </a>
+          </li>
+          <li>
+            <a title="instagram">
+              <img src={instagramIcon}></img>
+            </a>
+          </li>
+          <li>
+            <a title="twitter">
+              <img src={twitterIcon}></img>
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <form>
+          <input
+            type="email"
+            placeholder="Your Email Address"
+            className="subscribeEmail"
+          />
+          <input type="submit" value="Subscribe" className="subscribeButton" />
+        </form>
+      </section>
+    </footer>
+  );
 }
