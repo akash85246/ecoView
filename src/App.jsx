4b71bcca4989d6e552b52853c10a31182efb09af
Akash <sgc.akash.22oct.11sci@gmail.com>
2024-02-21 12:06:42 +0530

    Added products page with carousel slider, category filtering, and item display options
    This commit introduces a new Products page to the application. The page features a carousel slider showcasing sustainable and featured products, allowing users to navigate through slides with forward and backward buttons. Additionally, the page includes category cards for filtering items by clothes, furniture, utility, decor, and others. Users can toggle between grid and list views for item display, and sorting options based on popularity and price are implemented for enhanced user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { AuthProvider } from "./utils/authContext/authContext";
 import Home from "./component/home/home";
 import Scanner from "./component/scanner/scanner";
 import ImageScanner from "./component/imageScanner/imageScanner";
-import Store from "./component/store/store";
+import Products from "./component/products/products";
 import SignIn from "./component/authentication/signIn/signIn";
 import SignUp from "./component/authentication/signUp/signUp";
 import Verification from "./component/authentication/verification/verification";
@@ -24,7 +24,7 @@ export default function App() {
             <Route path="/signup" element={<SignUp />} />
             <Route path="/verification" element={<Verification />} />
             <Route path="/forget" element={<Forget />} />
-            <Route path="/store" element={<Store />} />
+            <Route path="/products" element={<Products />} />
             <Route path="/about" element={<About />} />
             <Route path="/profile" element={<Profile />} />
           </Routes>
--- src/component/store/store.jsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "../../utils/navbar/navbar";
-import "./store.css";
-export default function Store(props) {
-  return (
-    <>
-      <Navbar></Navbar>
-    </>
-  );
-}
